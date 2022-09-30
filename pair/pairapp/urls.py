ae223eb1afc169e27e192576a5b66f7f79198b82
Gkhy <syan1022@naver.com>
2022-09-30 13:43:10 +0900

    w

--- pair/pairapp/urls.py
+++ pair/pairapp/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = "pairapp"
 
 urlpatterns = [
-    path("", views.index, name='index'),
-    path("new/", views.new, name='new'),
-    path("create/", views.create, name='create'),
-    # path("detail/", views.detail, name='detail'),
+    path("", views.index, name="index"),
+    path("new/", views.new, name="new"),
+    path("create/", views.create, name="create"),
+    path("detail/<int:pk>", views.detail, name="detail"),
+    path("delete/<int:pk>", views.delete, name="delete"),
 ]
--- pair/pairapp/views.py
+++ pair/pairapp/views.py
@@ -11,12 +11,31 @@ def index(request):
 
     return render(request, "pairapp/index.html", context)
 
+
 def new(request):
     return render(request, "pairapp/new.html")
 
+
 def create(request):
-    title = request.GET.get('title')
-    content = request.GET.get('content')
+    title = request.GET.get("title")
+    content = request.GET.get("content")
     Review.objects.create(title=title, content=content)
 
-    return redirect('pairapp:index')
+    return redirect("pairapp:index")
+
+
+def detail(request, pk):
+    review = Review.objects.get(pk=pk)
+    pk_ = pk
+    context = {
+        "review": review,
+    }
+
+    return render(request, "pairapp/detail.html", context)
+
+
+def delete(request, pk):
+    review = Review.objects.get(pk=pk)
+    review.delete()
+
+    return redirect("pairapp:index")
